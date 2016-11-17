15a586fa9593c9bf0fceabfd4cf87e338280802a
erikwijmans <ewijmans2@gmail.com>
2016-11-17 21:34:48 +0000

    added batch files

--- testing/erik/header_parser.c
+++ testing/erik/header_parser.c
@@ -1,9 +1,19 @@
+#include <linux/elf.h>
 #include <stdio.h>
 
 int main() {
-  FILE * file = fopen("test", "r");
-  unsigned char magic[4];
-  fread((void *)magic, 1, 4, file);
+  FILE *file = fopen("test", "r");
+  struct elf64_hdr hdr;
+  fread(&hdr, 1, sizeof(struct elf64_hdr), file);
 
-  printf("%x %x %x %x \n", magic[0], magic[1], magic[2], magic[3]);
+  printf("%x %x %x %x\n", hdr.e_ident[EI_MAG0], hdr.e_ident[EI_MAG1],
+         hdr.e_ident[EI_MAG2], hdr.e_ident[EI_MAG3]);
+
+  printf("Entry point: %lx\n", hdr.e_phoff);
+
+  struct elf64_phdr phdr;
+  fseek(file, hdr.e_phoff, SEEK_CUR);
+  fread(&phdr, 1, sizeof(struct elf64_phdr), file);
+
+  printf("%lx\n", phdr.p_offset);
 }
