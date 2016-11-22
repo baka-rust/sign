ff2e3f1fc0e504e81d4a83bd67ddae232d3bc846
erikwijmans <ewijmans2@gmail.com>
2016-11-22 14:56:36 -0600

    commit b4 pull

--- testing/erik/header_parser.c
+++ testing/erik/header_parser.c
@@ -1,9 +1,19 @@
+#include <elf.h>
 #include <stdio.h>
 
 int main() {
-  FILE * file = fopen("test", "r");
-  unsigned char magic[4];
-  fread((void *)magic, 1, 4, file);
+  FILE *file = fopen("test", "r");
+  elfhdr hdr;
+  fread(&hdr, 1, sizeof(elfhdr), file);
 
-  printf("%x %x %x %x \n", magic[0], magic[1], magic[2], magic[3]);
+  printf("%x %x %x %x\n", hdr.e_ident[EI_MAG0], hdr.e_ident[EI_MAG1],
+         hdr.e_ident[EI_MAG2], hdr.e_ident[EI_MAG3]);
+
+  printf("Entry point: %lx\n", hdr.e_entry);
+
+  elf_phdr phdr;
+  fseek(file, hdr.e_phoff, SEEK_CUR);
+  fread(&phdr, 1, sizeof(elf_phdr), file);
+
+  printf("%lx\n", phdr.p_offset);
 }
