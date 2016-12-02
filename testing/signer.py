8cf71f90da9b7cf444f9bd772d46db5cb1414f33
Ethan Vaughan <ejvaughan@gmail.com>
2016-12-02 04:25:11 +0000

    Better signer.py

--- testing/signer.py
+++ testing/signer.py
@@ -8,20 +8,27 @@
 def check_if_shoud_sign(filename):
   with open(filename, "rb") as file:
     elf_magic = file.read(4)
-    is_elf = ord(elf_magic[0]) == 0x7f and elf_magic[1:] == "ELF"
+    is_elf = elf_magic == b'\x7fELF'
 
-    file.seek(-22, 2)
-    our_magic = file.read()
-    is_signed = our_magic.decode('utf-8') == "~~ BINARY SIGNATURE ~~"
+    if is_elf:
+      file.seek(-22, 2)
+      our_magic = file.read()
+      try:
+        is_signed = our_magic.decode('utf-8') == "~~ BINARY SIGNATURE ~~"
 
-    return is_elf and not is_signed
+        return not is_signed
+      except:
+        return False
+    else:
+      return False
 
 
 import subprocess, shlex
 from sys import argv
 
 if len(argv) != 4 and len(argv) != 5:
-  print("Usage: ./signer.py <path_to_binary> <path_to_cert> <path_to_private_key>")
+  print("Usage: ./signer.py <path_to_binary> <path_to_cert> <path_to_private_key> <private_key_password>")
+  print("\tIf the private key password is not supplied, the program will prompt the user to enter it")
   raise RuntimeError("Wrong number of arguments")
 
 binary_name = argv[1]
@@ -34,17 +41,19 @@ if check_if_shoud_sign(binary_name):
   else:
     command = "openssl cms -sign -signer {} -inkey {} -binary -in {} -outform der -noattr".format(cert_name, private_key_name, binary_name)
 
-  child =  subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+  child =  subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE)
   child.wait()
 
   if child.returncode != 0:
-    raise RuntimeError("Signer failed")
+    raise RuntimeError("openssl failed to sign")
 
   signature = child.communicate()[0]
 
   num_bytes = len(signature)
-
+  
   with open(binary_name, "ab") as f:
     f.write(signature)
     f.write(num_bytes.to_bytes(4, byteorder='little'))
     f.write("~~ BINARY SIGNATURE ~~".encode("utf-8"))
+
+  print("Signed")
