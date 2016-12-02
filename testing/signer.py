26324731bbe5cd72b77421e4f3ceebaaf28b3ab5
Ethan Vaughan <ejvaughan@gmail.com>
2016-12-02 03:51:35 +0000

    updated signer.py so it will only sign elf files (for now) and won't double sign stuff

--- testing/signer.py
+++ testing/signer.py
@@ -2,13 +2,25 @@
 
 '''
   Usage: ./signer.py <path_to_binary> <path_to_cert> <path_to_private_key> <private key password>
-	If <password> is not supplied, it will be prompted for
+  If <password> is not supplied, it will be prompted for
 '''
+
+def check_if_shoud_sign(filename):
+  with open(filename, "rb") as file:
+    elf_magic = file.read(4)
+    is_elf = ord(elf_magic[0]) == 0x7f and elf_magic[1:] == "ELF"
+
+    file.seek(-22, 2)
+    our_magic = file.read()
+    is_signed = our_magic.decode('utf-8') == "~~ BINARY SIGNATURE ~~"
+
+    return is_elf and not is_signed
+
 
 import subprocess, shlex
 from sys import argv
 
-if len(argv) != 4:
+if len(argv) != 4 and len(argv) != 5:
   print("Usage: ./signer.py <path_to_binary> <path_to_cert> <path_to_private_key>")
   raise RuntimeError("Wrong number of arguments")
 
@@ -16,22 +28,23 @@ binary_name = argv[1]
 cert_name = argv[2]
 private_key_name = argv[3]
 
-if len(argv) == 5:
-  command = "openssl cms -sign -signer {} -inkey {} -binary -in {} -outform der -noattr -passin pass:{}".format(cert_name, private_key_name, binary_name, argv[4])
-else:
-  command = "openssl cms -sign -signer {} -inkey {} -binary -in {} -outform der -noattr".format(cert_name, private_key_name, binary_name)
+if check_if_shoud_sign(binary_name):
+  if len(argv) == 5:
+    command = "openssl cms -sign -signer {} -inkey {} -binary -in {} -outform der -noattr -passin pass:{}".format(cert_name, private_key_name, binary_name, argv[4])
+  else:
+    command = "openssl cms -sign -signer {} -inkey {} -binary -in {} -outform der -noattr".format(cert_name, private_key_name, binary_name)
 
-child =  subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE, stdin=subprocess.PIPE)
-child.wait()
+  child =  subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+  child.wait()
 
-if child.returncode != 0:
-  raise RuntimeError("Signer failed")
+  if child.returncode != 0:
+    raise RuntimeError("Signer failed")
 
-signature = child.communicate()[0]
+  signature = child.communicate()[0]
 
-num_bytes = len(signature)
+  num_bytes = len(signature)
 
-with open(binary_name, "ab") as f:
-  f.write(signature)
-  f.write(num_bytes.to_bytes(4, byteorder='little'))
-  f.write("~~ BINARY SIGNATURE ~~".encode("utf-8"))
+  with open(binary_name, "ab") as f:
+    f.write(signature)
+    f.write(num_bytes.to_bytes(4, byteorder='little'))
+    f.write("~~ BINARY SIGNATURE ~~".encode("utf-8"))
